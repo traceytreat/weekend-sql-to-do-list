19add8254beb4bb4aa6c5a474ba9fc2790db5040
Tracey <treatracey@gmail.com>
2022-11-13 15:05:14 -0600

    Record date created and date completed.

--- database.sql
+++ database.sql
@@ -3,10 +3,11 @@ CREATE TABLE tasks_list (
     "name" varchar(30) NOT NULL,
     "description" varchar(250),
     "date" varchar(45) NOT NULL,
+    "datecompleted" varchar(45),
     "completed" boolean NOT NULL
 );
 
-INSERT INTO tasks_list ("name", "description", "date", "completed")
+INSERT INTO tasks_list ("name", "description", "date", "datecompleted", "completed")
 VALUES 
-('Do Homework', 'Finish up the homework due this weekend.', 'November 11th 2022, 5:00:00 pm', FALSE),
-('Clean Bedroom', 'Clean up my messy bedroom', 'November 10th 2022, 7:30:00 pm', TRUE);
+('Do Homework', 'Finish up the homework due this weekend.', 'November 11th 2022, 5:00:00 pm', '', FALSE),
+('Clean Bedroom', 'Clean up my messy bedroom', 'November 10th 2022, 7:30:00 pm', 'November 12th 2022, 5:00:00 pm', TRUE);
